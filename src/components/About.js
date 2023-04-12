d1641f53747093868f782076187e241964d87f40
Tautik Agrahari <tautikagrahari@gmail.com>
2023-04-12 15:55:04 +0530

    Added Tailwind css and Profile Class removed instead used Profile function

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,13 @@
 import Profile from "./Profile";
+import Random from "./Random";
 const About = () => {
   return (
     <div>
       <h1>WELCOME TO ABOUT SECTION</h1>
-      <Profile />
+      <div className=" flex border">
+        <Profile />
+        <Random />
+      </div>
     </div>
   );
 };
---
+++ src/components/Random.js
@@ -0,0 +1,11 @@
+const Random = () => {
+  return (
+    <div className=" flex-1 bg-red-300">
+      <h2>dskfh</h2>
+      <h2>dskfh</h2>
+      <h2>dskfh</h2>
+      <h2>dskfh</h2>
+    </div>
+  );
+};
+export default Random;
